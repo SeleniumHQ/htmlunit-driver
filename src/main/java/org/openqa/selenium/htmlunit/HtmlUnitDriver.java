05f4f4cb8afa3668661ce982763ad31df24a2f1d
Alexei Barantsev <barancev@gmail.com>
2016-02-21 02:31:05 +0300

    Fixing use of deprecated method

--- src/main/java/org/openqa/selenium/htmlunit/HtmlUnitDriver.java
+++ src/main/java/org/openqa/selenium/htmlunit/HtmlUnitDriver.java
@@ -688,7 +688,7 @@ public class HtmlUnitDriver implements WebDriver, JavascriptExecutor,
     try {
       result = page.executeJavaScriptFunctionIfPossible(
           func,
-          (ScriptableObject) getCurrentWindow().getScriptObject(),
+          (ScriptableObject) getCurrentWindow().getScriptableObject(),
           parameters,
           page.getDocumentElement());
     } catch (Throwable ex) {
@@ -710,7 +710,7 @@ public class HtmlUnitDriver implements WebDriver, JavascriptExecutor,
   }
 
   private Object[] convertScriptArgs(HtmlPage page, final Object[] args) {
-    final Scriptable scope = (Scriptable) page.getEnclosingWindow().getScriptObject();
+    final Scriptable scope = (Scriptable) page.getEnclosingWindow().getScriptableObject();
 
     final Object[] parameters = new Object[args.length];
     final ContextAction action = new ContextAction() {
@@ -766,12 +766,12 @@ public class HtmlUnitDriver implements WebDriver, JavascriptExecutor,
     if (arg instanceof HtmlUnitWebElement) {
       HtmlUnitWebElement webElement = (HtmlUnitWebElement) arg;
       assertElementNotStale(webElement.getElement());
-      return webElement.getElement().getScriptObject();
+      return webElement.getElement().getScriptableObject();
 
     } else if (arg instanceof HtmlElement) {
       HtmlElement element = (HtmlElement) arg;
       assertElementNotStale(element);
-      return element.getScriptObject();
+      return element.getScriptableObject();
 
     } else if (arg instanceof Collection<?>) {
       List<Object> list = new ArrayList<>();
