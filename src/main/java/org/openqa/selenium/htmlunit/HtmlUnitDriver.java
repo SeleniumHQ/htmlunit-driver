769c9ad59c7e9ad9454072991cbfe366c2d66ca2
Ahmed Ashour <asashour@yahoo.com>
2017-02-10 13:43:55 +0100

    refector

--- src/main/java/org/openqa/selenium/htmlunit/HtmlUnitDriver.java
+++ src/main/java/org/openqa/selenium/htmlunit/HtmlUnitDriver.java
@@ -251,11 +251,7 @@ public class HtmlUnitDriver implements WebDriver, JavascriptExecutor,
 
     setProxySettings(Proxy.extractFrom(capabilities));
 
-    setDownloadImages(isDownloadImages(capabilities));
-  }
-
-  private static boolean isDownloadImages(Capabilities capabilities) {
-    return capabilities.is(DOWNLOAD_IMAGES_CAPABILITY);
+    setDownloadImages(capabilities.is(DOWNLOAD_IMAGES_CAPABILITY));
   }
 
   public HtmlUnitDriver(Capabilities desiredCapabilities, Capabilities requiredCapabilities) {
