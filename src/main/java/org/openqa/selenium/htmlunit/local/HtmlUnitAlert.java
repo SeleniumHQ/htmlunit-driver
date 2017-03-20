43403dd6a5e57172e01c7862439a97f7b91fd2ac
Ahmed Ashour <asashour@yahoo.com>
2017-03-20 09:46:25 +0100

    Cleanup

--- src/main/java/org/openqa/selenium/htmlunit/local/HtmlUnitAlert.java
+++ src/main/java/org/openqa/selenium/htmlunit/local/HtmlUnitAlert.java
@@ -28,17 +28,12 @@ import org.openqa.selenium.security.Credentials;
 
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebWindow;
 
 public class HtmlUnitAlert implements Alert {
 
   HtmlUnitLocalDriver driver;
   private AlertHolder holder_;
   private boolean quitting_;
-//  private String message;
-//  private String promptKeys;
-//  private String promptValue;
-//  private boolean accepted;
   private Lock l = new ReentrantLock();
   private Condition c = l.newCondition();
 
@@ -55,27 +50,10 @@ public class HtmlUnitAlert implements Alert {
       return;
     }
     l.lock();
-    try {
-      holder_ = new AlertHolder(page.getEnclosingWindow(), message);
-//    this.message = message;
-//    accepted = false;
+    holder_ = new AlertHolder(message);
     boolean proceed = driver.alert();
     if (proceed) {
       c.awaitUninterruptibly();
-    }
-    l.unlock();
-//    System.out.println("alertHandler 5");
-//
-//    synchronized (lock_) {
-//      try {
-//        lock_.wait();
-//      } catch (InterruptedException e) {
-//        throw new IllegalStateException(e);
-//      }
-//    }
-//    close();
-    }catch(Exception e) {
-      e.printStackTrace();
     }
   }
 
@@ -83,57 +61,26 @@ public class HtmlUnitAlert implements Alert {
     if (quitting_) {
       return null;
     }
-    holder_ = new PromptHolder(page.getEnclosingWindow(), message, defaultMessage);
+    holder_ = new PromptHolder(message, defaultMessage);
     PromptHolder localHolder = (PromptHolder) holder_;
-//    this.message = message;
-//    this.promptValue = message;
-//    accepted = false;
     l.lock();
     driver.alert();
     c.awaitUninterruptibly();
     l.unlock();
     return localHolder.value;
-//    lock_ = new PromptLock(page.getEnclosingWindow(), message, defaultMessage);
-//
-//    synchronized (lock_) {
-//      try {
-//        lock_.wait();
-//      } catch (InterruptedException e) {
-//        throw new IllegalStateException(e);
-//      }
-//    }
-//    String value = ((PromptLock) lock_).value;
-//    close();
-//    return value;
   }
 
   private boolean onbeforeunloadHandler(Page page, String returnValue) {
     if (quitting_) {
       return true;
     }
-    holder_ = new AlertHolder(page.getEnclosingWindow(), returnValue);
+    holder_ = new AlertHolder(returnValue);
     AlertHolder localHolder = holder_;
-//    this.message = returnValue;
-//    accepted = false;
     l.lock();
     driver.alert();
     c.awaitUninterruptibly();
     l.unlock();
     return localHolder.isAccepted();
-//    lock_ = new AlertLock(page.getEnclosingWindow(), returnValue);
-//
-//    if (!autoAccept_) {
-//      synchronized (lock_) {
-//        try {
-//          lock_.wait();
-//        } catch (InterruptedException e) {
-//          throw new IllegalStateException(e);
-//        }
-//      }
-//    }
-//    boolean accepted = lock_.isAccepted();
-//    close();
-//    return accepted;
   }
 
   public void setAutoAccept(boolean autoAccept) {
@@ -143,69 +90,31 @@ public class HtmlUnitAlert implements Alert {
   @Override
   public void dismiss() {
     l.lock();
-//    message = null;
-//    promptValue = null;
-//    accepted = false;
     c.signal();
     holder_ = null;
     l.unlock();
-//    if (lock_ == null) {
-//      throw new NoAlertPresentException();
-//    }
-//    synchronized (lock_) {
-//      lock_.notify();
-//    }
   }
 
   @Override
   public void accept() {
     l.lock();
     holder_.accept();
-//    message = null;
-//    accepted = true;
     c.signal();
     holder_ = null;
     l.unlock();
-//    if (lock_ == null) {
-//      throw new NoAlertPresentException();
-//    }
-//    lock_.accept();
-//    synchronized (lock_) {
-//      lock_.notify();
-//    }
   }
 
   @Override
   public String getText() {
-//    boolean locked = l.tryLock();
-//    System.out.println("locked = " + locked);
-//    System.out.println("message " + message);
-//    if (locked) {
-//      l.unlock();
-//      throw new NoAlertPresentException();
-//    }
-//    System.out.println("getText " + message);
     if (holder_ == null) {
       throw new NoAlertPresentException();
     }
     return holder_.message;
-//    if (message == null)
-//    if (lock_ == null) {
-//      throw new NoAlertPresentException();
-//    }
-//    if (lock_.webWindow != driver.getCurrentWindow()) {
-//      throw new AssertionError();
-//    }
-//    return lock_.message;
   }
 
   @Override
   public void sendKeys(String keysToSend) {
     holder_.sendKeys(keysToSend);
-//    if (lock_ == null) {
-//      throw new NoAlertPresentException();
-//    }
-//    lock_.sendKeys(keysToSend);
   }
 
   @Override
@@ -232,12 +141,10 @@ public class HtmlUnitAlert implements Alert {
   }
 
   private static class AlertHolder {
-    WebWindow webWindow;
     String message;
     boolean accepted;
 
-    AlertHolder(WebWindow webWindow, String message) {
-      this.webWindow = webWindow;
+    AlertHolder(String message) {
       this.message = message;
     }
 
@@ -261,8 +168,8 @@ public class HtmlUnitAlert implements Alert {
     String defaultMessage;
     String value;
 
-    public PromptHolder(WebWindow webWindow, String message, String defaultMessage) {
-      super(webWindow, message);
+    public PromptHolder(String message, String defaultMessage) {
+      super(message);
       this.defaultMessage = defaultMessage;
     }
   
