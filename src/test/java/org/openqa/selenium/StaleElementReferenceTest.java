8f4bb984e1b7f8534a22b9f369a3f986c58e8084
Ahmed Ashour <asashour@yahoo.com>
2017-03-31 17:10:59 +0200

    Add tests

---
+++ src/test/java/org/openqa/selenium/StaleElementReferenceTest.java
@@ -0,0 +1,84 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
+
+import org.junit.Test;
+import org.openqa.selenium.testing.JUnit4TestBase;
+import org.openqa.selenium.testing.JavascriptEnabled;
+
+public class StaleElementReferenceTest extends JUnit4TestBase {
+
+  @Test
+  public void testOldPage() {
+    driver.get(pages.simpleTestPage);
+    WebElement elem = driver.findElement(By.id("links"));
+    driver.get(pages.xhtmlTestPage);
+    try {
+      elem.click();
+      fail();
+    } catch (StaleElementReferenceException e) {
+      // do nothing. this is what we expected.
+    }
+  }
+
+  @JavascriptEnabled
+  @Test
+  public void testShouldNotCrashWhenCallingGetSizeOnAnObsoleteElement() {
+    driver.get(pages.simpleTestPage);
+    WebElement elem = driver.findElement(By.id("links"));
+    driver.get(pages.xhtmlTestPage);
+    try {
+      elem.getSize();
+      fail();
+    } catch (StaleElementReferenceException e) {
+      // do nothing. this is what we expected.
+    }
+  }
+
+  @JavascriptEnabled
+  @Test
+  public void testShouldNotCrashWhenQueryingTheAttributeOfAStaleElement() {
+    driver.get(pages.xhtmlTestPage);
+    WebElement heading = driver.findElement(By.xpath("//h1"));
+    driver.get(pages.simpleTestPage);
+    try {
+      heading.getAttribute("class");
+      fail();
+    } catch (StaleElementReferenceException e) {
+      // do nothing. this is what we expected.
+    }
+  }
+
+  @JavascriptEnabled
+  @Test
+  public void testRemovingAnElementDynamicallyFromTheDomShouldCauseAStaleRefException() {
+    driver.get(pages.javascriptPage);
+
+    WebElement toBeDeleted = driver.findElement(By.id("deleted"));
+    assertTrue(toBeDeleted.isDisplayed());
+
+    driver.findElement(By.id("delete")).click();
+
+    boolean wasStale = wait.until(stalenessOf(toBeDeleted));
+    assertTrue("Element should be stale at this point", wasStale);
+  }
+}
