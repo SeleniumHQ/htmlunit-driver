f3cba476e9de3aa5fbcc419edefe749c05c55312
rbri <rbri@rbri.de>
2019-09-28 13:25:16 +0200

    switch to the new getText() implementation

--- src/main/java/org/openqa/selenium/htmlunit/HtmlUnitWebElement.java
+++ src/main/java/org/openqa/selenium/htmlunit/HtmlUnitWebElement.java
@@ -463,7 +463,7 @@ public class HtmlUnitWebElement implements WrapsDriver,
   @Override
   public String getText() {
     assertElementNotStale();
-    return element.asText();
+    return element.getText();
   }
 
   protected HtmlUnitDriver getParent() {
