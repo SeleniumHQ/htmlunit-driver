70d89e5f6eff3329db2127b5f3948944780f2b5b
Ronald Brill <rbri@rbri.de>
2025-10-10 13:39:57 +0200

    format the code a bit

--- src/main/java/org/openqa/selenium/htmlunit/AsyncScriptExecutor.java
+++ src/main/java/org/openqa/selenium/htmlunit/AsyncScriptExecutor.java
@@ -96,8 +96,10 @@ class AsyncScriptExecutor {
 
     private Function createInjectedScriptFunction(final String userScript, final AsyncScriptResult asyncResult) {
         final String script =
-                "let huDriverAsyncFoo = function() {"
+                "let huDriverAsyncFoo = "
+                + "function() {"
                 + "  var self = this, timeoutId;"
+
                 + "  var cleanUp = function() {"
                 + "    window.clearTimeout(timeoutId);"
                 + "    if (window.detachEvent) {"
@@ -106,16 +108,19 @@ class AsyncScriptExecutor {
                 + "      window.removeEventListener('unload', catchUnload, false);"
                 + "    }"
                 + "  };"
+
                 + "  var self = this, timeoutId, catchUnload = function() {"
                 + "    cleanUp();"
                 + "    self.host.unload();"
                 + "  };"
+
                 // Convert arguments into an actual array, then add the callback object.
                 + "  arguments = Array.prototype.slice.call(arguments, 0);"
                 + "  arguments.push(function(value) {"
                 + "    cleanUp();"
                 + "    self.host.callback(typeof value == 'undefined' ? null : value);"
                 + "  });"
+
                 // Add an event listener to trap unload events; page loads are not supported
                 // with async script execution.
                 + "  if (window.attachEvent) {"
@@ -123,19 +128,16 @@ class AsyncScriptExecutor {
                 + "  } else {"
                 + "    window.addEventListener('unload', catchUnload, false);"
                 + "  }"
+
                 // Execute the user's script
-                + "  (function() {"
-                + userScript
-                + "}).apply(null, arguments);"
+                + "  (function() {" + userScript + "}).apply(null, arguments);"
+
                 // Register our timeout for the script. If the script invokes the callback
                 // immediately (e.g. it's not really async), then this will still fire.
                 // That's OK because the host object should ignore the extra timeout.
-                + "  timeoutId = window.setTimeout(function() {"
-                + "    self.host.timeout();"
-                + "  }, "
-                + timeoutMillis_
-                + ");"
-                + "}; huDriverAsyncFoo;";
+                + "  timeoutId = window.setTimeout(function() { self.host.timeout(); }, " + timeoutMillis_ + ");"
+                + "}; "
+                + "huDriverAsyncFoo;";
 
         // Compile our script.
         final ScriptResult result = page_.executeJavaScript(script);
